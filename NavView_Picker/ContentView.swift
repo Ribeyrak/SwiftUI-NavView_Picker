2cb928d5e5afc5818a4878a9484f57b3ce87a323
Evhen <35118152+Ribeyrak@users.noreply.github.com>
2024-08-14 22:41:03 -0400

    Initial Commit

---
+++ NavView_Picker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavView_Picker
+//
+//  Created by Evhen Lukhtan on 14.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavView_Picker/NavView_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavView_PickerApp.swift
+//  NavView_Picker
+//
+//  Created by Evhen Lukhtan on 14.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavView_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
